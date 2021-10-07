b42f05b0f3ec468cfe72cfd5fd90bca1a04c8f29
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-10-07 15:46:19 -0400

    lets try writing smaller fuck yeah

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ while j < j_index:
         else:
             LB = 0
             UB = float(lowerbound[1])
-        scaler = 1.2
+        scaler = 0.25
         LB = float(scaler*LB)
         UB = float(scaler*UB)
 
