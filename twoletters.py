b4d9c449d30b3d0356d72c51db0e0b145637b289
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-10-12 13:20:39 -0400

    hi

--- twoletters.py
+++ twoletters.py
@@ -22,7 +22,7 @@ kit.servo[3].angle = 0
 initials = (input('Please input what you want to write: '))
 # defines the scaler variable, defines the size of the letter you are writing
         # going below 0 is not recommended
-scaler = 3
+scaler = 2
 # offset of the letters is based on the number of letters you want to write
 offset = -0.5
 #defines basic turtle display variables
